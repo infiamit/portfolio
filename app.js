daab08a18b942942200347574b7cc9602b878c48
infiamit <amitsingh80108@gmail.com>
2018-07-15 19:48:33 +0530

    chat.js

--- app.js
+++ app.js
@@ -7,8 +7,9 @@ var expressValidator=require('express-validator');
 var express = require('express');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+ 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -39,7 +40,9 @@ app.use(expressValidator({
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-
+app.get('/chat', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -58,3 +61,7 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+ 
+ 
+ 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var ContactForm = require('../models/contactform');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
  
@@ -8,32 +8,6 @@ router.get('/', function(req, res, next) {
 });
 
 
-router.post('/', function(req,res,next){
-  var name=req.body.name;
-  var email=req.body.email;
-  var phone=req.body.phone;
-  var message=req.body.message;
-  
-  req.checkBody('name','Email Required').notEmpty();
-  req.checkBody('email','Wrong Email Address').isEmail();
-  req.checkBody('phone','Invalid Phone Number').notEmpty();
-  req.checkBody('message','Message Field Required').notEmpty();
-
-  var error=req.validationErrors();
-  if(error){
-    res.render('index', { error: error ,title: 'My Portfolio' });
-  }else{
-    var newContact= new ContactForm({
-      name:name,
-       email:email,
-       phone:phone,
-      message:message
-    });
-    newContact.save();
-    console.log(process.env.dbpassword);
-  }
-  
-  res.render('index', { title: 'My Portfolio', success:'Your Message Successfully Received...' });
-});
+router.post('/', require('../controllers/contactForm'));
 
 module.exports = router;
