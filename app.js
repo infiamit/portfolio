1da71686590079aaf2674154727cab290c4b85ca
infiamit <amitsingh80108@gmail.com>
2018-07-20 01:34:59 +0530

    compress

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.use(compression())
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('view cache', true);
+
 
 app.use(logger('dev'));
 app.use(express.json());
